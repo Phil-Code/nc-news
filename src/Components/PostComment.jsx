ca34b614be90d824975b638c7748dd48750c7d15
Phil-Code <pjdixon@hotmail.co.uk>
2023-10-26 16:46:35 +0100

    tidied up PostComment

--- src/Components/PostComment.jsx
+++ src/Components/PostComment.jsx
@@ -1,41 +1,26 @@
-import { useEffect, useState, useContext } from "react";
+import { useState, useContext } from "react";
 import { useParams } from "react-router-dom";
-import { fetchUsers, postComment } from "../utils";
 import LoginBar from "./LoginBar";
 import { LoginContext } from "./Contexts/LoginContext";
+import { postComment } from "../utils";
 
 export default function PostComment({setIsPosting, setComments, setPostingErr, setCommentCount }){
 
     const {article_id} = useParams()
-    const [name, setName] = useState('')
     const [body, setBody] = useState('')
-    const [isValidName, setIsValidName] = useState(false)
     const [isValidComment, setIsValidComment] = useState(false)
-    const [users, setUsers] = useState([])
     const {login, setLogin} = useContext(LoginContext)
-
-    useEffect(()=>{
-        fetchUsers()
-        .then((result)=>{
-            const usernames = [];
-            result.forEach((user)=>{
-                usernames.push(user.username)
-            })
-            setUsers(usernames)
-        })
-    }, [])
 
     function handleSubmit(e){
         e.preventDefault()
         setPostingErr(false)
-        if (isValidName && isValidComment){
+        if (isValidComment){
             setComments((current)=>{
-                return [{"author": name, "body": body, "votes": 0, newPost: true}, ...current]
+                return [{"author": login, "body": body, "votes": 0, newPost: true}, ...current]
             })
-            setName('')
             setBody('')
             setIsPosting(false)
-            postComment(article_id, {"username": name, "body": body})
+            postComment(article_id, {"username": login, "body": body})
             .then(()=>{
                 setCommentCount((current)=>{
                     return current + 1
@@ -52,12 +37,6 @@ export default function PostComment({setIsPosting, setComments, setPostingErr, s
                 })
             })        
         }
-    }
-    function handleNameChange(e){
-        setName(e.target.value)
-        if (users.includes(e.target.value)){
-            setIsValidName(true)
-        } else setIsValidName(false)
     }
 
     function handleBodyChange(e){
@@ -76,13 +55,6 @@ export default function PostComment({setIsPosting, setComments, setPostingErr, s
 }
     return (
         <form onSubmit={handleSubmit} className="comment-form">
-            <div className="name-input">
-            <label htmlFor="name">Your name: </label>
-            <input id='name' value={name} onChange={handleNameChange} type="text" />
-            </div>
-            <div className="name-feedback">
-                {isValidName ? '' : <p>please enter a valid username (try grumpy19)</p>}
-            </div>
             <div className="comment-input">
             <label htmlFor="body">Your comment: </label>
             <input id='body' value={body} onChange={handleBodyChange} type="text" />
